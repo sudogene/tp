b98819278547e585ec9f6f6b73f8997deed3854d
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-09-23 01:46:31 +0800

    Delete unused code

--- src/test/java/seedu/address/logic/commands/FindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindCommandTest.java
@@ -30,7 +30,6 @@ public class FindCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-
     @Test
     public void equals() {
         NameContainsKeywordsPredicate firstNamePredicate =
--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,7 +1,5 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import java.util.Arrays;
 
@@ -15,14 +13,6 @@ import seedu.address.model.student.PredicateList;
 public class FindCommandParserTest {
 
     private FindCommandParser parser = new FindCommandParser();
-
-
-    /* TODO: Do you want to have emptyArgs throw exception? PM me!
-    @Test
-    public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
-    }
-    */
 
     @Test
     public void parse_validArgs_returnsFindCommand() {
