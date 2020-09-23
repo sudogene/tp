c19d486eabe3680cabfd8032699c6925902e577b
sudogene <andywubby@gmail.com>
2020-09-23 20:03:41 +0800

    Add methods to FindCommandParserTest

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -16,20 +16,11 @@ public class FindCommandParserTest {
     private FindCommandParser parser = new FindCommandParser();
 
     @Test
-    public void parse_validMultipleArgs_returnsFindCommand() {
-        // no leading and trailing whitespaces
-        FindCommand firstExpectedFindCommand =
-                new FindCommand(new PredicateList(Arrays.asList(
-                        new NameContainsKeywordsPredicate(Arrays.asList("Alice", "Bob")),
-                        new PhoneMatchesPredicate("123456"))));
-        assertParseSuccess(parser, " n/Alice Bob p/123456", firstExpectedFindCommand);
+    public void parse_oneFieldSpecified_returnsFindCommand() {
+    }
 
-        // multiple whitespaces between keywords
-        FindCommand secondExpectedFindCommand =
-                new FindCommand(new PredicateList(Arrays.asList(
-                        new NameContainsKeywordsPredicate(Arrays.asList("Alice")),
-                        new PhoneMatchesPredicate("85355255"))));
-        assertParseSuccess(parser, "  \n  \t n/Alice \t \n \n p/85355255", secondExpectedFindCommand);
+    @Test
+    public void parse_multipleFieldsSpecified_returnsFindCommand() {
     }
 
     @Test
