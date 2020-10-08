1ec620f3c55f609678744ce78df41f3c2b5d4c9d
Lee Wei Heng <leeweiheng@WHs-MacBook-Pro.local>
2020-10-08 23:53:19 +0800

    Add Training class

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -41,6 +41,10 @@ public class AddressBook implements ReadOnlyAddressBook {
         resetData(toBeCopied);
     }
 
+    public Set<Training> getTrainings() {
+        return this.trainings;
+    }
+
     //// list overwrite operations
 
     /**
