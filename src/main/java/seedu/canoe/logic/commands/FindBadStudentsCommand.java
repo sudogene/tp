859b1a1596425691aaaec94712cea7b85a12a2b6
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-10-27 18:20:49 +0800

    Add findbadstudentscommand

---
+++ src/main/java/seedu/canoe/logic/commands/FindBadStudentsCommand.java
@@ -0,0 +1,49 @@
+package seedu.canoe.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import seedu.canoe.commons.core.LogsCenter;
+import seedu.canoe.model.Model;
+import seedu.canoe.model.student.AnyMatchPredicateList;
+import seedu.canoe.model.student.Student;
+
+public class FindBadStudentsCommand extends Command {
+
+    public static final Logger LOGGER = LogsCenter.getLogger(FindBadStudentsCommand.class);
+
+    public static final String COMMAND_WORD = "find-bad-students";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Finds all students who have missed more than 3 training "
+            + "sessions and displays them as a list.\n"
+            + "Example: " + COMMAND_WORD;
+
+    private static final String NO_BAD_STUDENTS_MESSAGE = "No students with a bad attendance record were found!";
+
+    private List<Student> badStudents;
+
+    @Override
+    public CommandResult execute(Model model) {
+        LOGGER.info("=============================[ Executing FindBadStudentsCommand ]===========================");
+        requireNonNull(model);
+
+        model.updateFilteredStudentList(Model.PREDICATE_SHOW_ALL_STUDENTS);
+        List<Student> studentList = model.getFilteredStudentList();
+
+        for (Student student : studentList) {
+            if (student.hasBadAttendanceRecord()) {
+                badStudents.add(student);
+            }
+        }
+
+        if (badStudents.isEmpty()) {
+            LOGGER.info("No students with a bad attendance record were found!");
+            return new CommandResult(NO_BAD_STUDENTS_MESSAGE);
+        }
+
+        return new CommandResult(badStudents.toString());
+    }
+}
