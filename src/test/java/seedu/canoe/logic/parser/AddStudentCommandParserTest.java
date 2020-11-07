e3349c88a80a31d6962d79885403f773d4ff619f
Lee Wei Heng <leeweiheng@WHs-MacBook-Pro.local>
2020-11-07 14:45:27 +0800

    Fix JUnit test names

--- src/test/java/seedu/canoe/logic/parser/AddStudentCommandParserTest.java
+++ src/test/java/seedu/canoe/logic/parser/AddStudentCommandParserTest.java
@@ -15,27 +15,27 @@ public class AddStudentCommandParserTest {
     private AddStudentCommandParser parser = new AddStudentCommandParser();
 
     @Test
-    public void parse_noStudents_failure() throws Exception {
+    public void parseNoStudentsFailure() throws Exception {
         assertParseFailure(parser, "1 id/", ParserUtil.MESSAGE_NO_ID_PROVIDED);
     }
 
     @Test
-    public void parse_nonNumericStudent_failure() {
+    public void parseNonNumericStudentFailure() {
         assertParseFailure(parser, "1 id/abc", Id.MESSAGE_CONSTRAINTS);
     }
 
     @Test
-    public void parse_repeatedStudents_failure() {
+    public void parseRepeatedStudentsFailure() {
         assertParseFailure(parser, "1 id/1,1,1,1,1,1", ParserUtil.MESSAGE_REPEATED_ID);
     }
 
     @Test
-    public void parse_singleValidStudent_success() throws Exception {
+    public void parseSingleValidStudentSuccess() throws Exception {
         assertParseSuccess(parser, "1 id/2", new AddStudentToTrainingCommand(INDEX_FIRST_TRAINING, VALID_ID_LIST_2));
     }
 
     @Test
-    public void parse_multipleValidStudents_success() throws Exception {
+    public void parseMultipleValidStudentsSuccess() throws Exception {
         assertParseSuccess(parser, "1 id/1,2,3",
                 new AddStudentToTrainingCommand(INDEX_FIRST_TRAINING, VALID_ID_LIST_6));
     }
