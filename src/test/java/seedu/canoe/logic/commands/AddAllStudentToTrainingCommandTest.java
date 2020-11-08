b6585d997cdc4ccec0d1184055ee2fcde50404ae
abdurrahmanfaqihiskandar <e0052804@u.nus.edu>
2020-11-08 20:54:20 +0800

    Add addallstudentcommandparser test

--- src/test/java/seedu/canoe/logic/commands/AddAllStudentToTrainingCommandTest.java
+++ src/test/java/seedu/canoe/logic/commands/AddAllStudentToTrainingCommandTest.java
@@ -5,11 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.canoe.testutil.Assert.assertThrows;
 import static seedu.canoe.testutil.LocalDateTimeUtil.VALID_LOCAL_DATE_TIME_2;
+import static seedu.canoe.testutil.LocalDateTimeUtil.VALID_LOCAL_DATE_TIME_3;
 import static seedu.canoe.testutil.TypicalIndexes.INDEX_FIFTH_TRAINING;
 import static seedu.canoe.testutil.TypicalIndexes.INDEX_FIRST_TRAINING;
-import static seedu.canoe.testutil.TypicalIndexes.INDEX_FOURTH_TRAINING;
 import static seedu.canoe.testutil.TypicalIndexes.INDEX_SECOND_TRAINING;
 import static seedu.canoe.testutil.TypicalStudents.BOB;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,14 +23,17 @@ import seedu.canoe.model.ModelManager;
 import seedu.canoe.model.UserPrefs;
 import seedu.canoe.model.student.AllMatchPredicateList;
 import seedu.canoe.model.student.Attendance;
+import seedu.canoe.model.student.Id;
 import seedu.canoe.model.student.Student;
 import seedu.canoe.model.training.Training;
 import seedu.canoe.testutil.StudentBuilder;
-import seedu.canoe.testutil.TypicalStudentsInTypicalTrainings;
+import seedu.canoe.testutil.TrainingBuilder;
+import seedu.canoe.testutil.TypicalStudents;
+import seedu.canoe.testutil.TypicalTraining;
 
 public class AddAllStudentToTrainingCommandTest {
 
-    private Model model = new ModelManager(TypicalStudentsInTypicalTrainings.getTypicalAddressBook(), new UserPrefs());
+    private Model model = new ModelManager(TypicalStudents.getTypicalAddressBook(), new UserPrefs());
 
     public Model getModel() {
         return model;
@@ -41,9 +47,11 @@ public class AddAllStudentToTrainingCommandTest {
 
     @Test
     public void isAbleToAddStudent() {
-        Training training = model.getFilteredTrainingList().get(3);
+        getModel().addTraining(TypicalTraining.VALID_PAST_TRAINING);
+        Training training = model.getFilteredTrainingList().get(0);
         Student student = new StudentBuilder(BOB).build();
         assertTrue(AddAllStudentToTrainingCommand.isAbleToAddStudent(student, training));
+        getModel().deleteTraining(TypicalTraining.VALID_PAST_TRAINING);
     }
 
     @Test
@@ -58,6 +66,7 @@ public class AddAllStudentToTrainingCommandTest {
     public void execute_emptyStudentList_throwsCommandException() throws Exception {
         AddAllStudentToTrainingCommand addAllStudentToTrainingCommand =
                 new AddAllStudentToTrainingCommand(INDEX_FIRST_TRAINING);
+        getModel().addTraining(TypicalTraining.VALID_TRAINING);
         getModel().updateFilteredStudentList(new AllMatchPredicateList());
         assertThrows(CommandException.class, addAllStudentToTrainingCommand.MESSAGE_NO_STUDENTS, () ->
                 addAllStudentToTrainingCommand.execute(getModel()));
@@ -67,6 +76,8 @@ public class AddAllStudentToTrainingCommandTest {
     public void execute_studentAddToPastTraining_addFail() throws Exception {
         AddAllStudentToTrainingCommand addAllStudentToTrainingCommand =
                 new AddAllStudentToTrainingCommand(INDEX_SECOND_TRAINING);
+        getModel().addTraining(TypicalTraining.VALID_PAST_TRAINING);
+        getModel().addTraining(TypicalTraining.VALID_PAST_TRAINING2);
         getModel().updateFilteredStudentList(Model.PREDICATE_SHOW_ALL_STUDENTS);
         assertThrows(CommandException.class, AddAllStudentToTrainingCommand.MESSAGE_TRAINING_CANNOT_ADD, () ->
                 addAllStudentToTrainingCommand.execute(getModel()));
@@ -75,10 +86,12 @@ public class AddAllStudentToTrainingCommandTest {
     @Test
     public void execute_studentsAcceptedByModel_addSuccessful() throws Exception {
         AddAllStudentToTrainingCommand addAllStudentToTrainingCommand =
-                new AddAllStudentToTrainingCommand(INDEX_FOURTH_TRAINING);
+                new AddAllStudentToTrainingCommand(INDEX_SECOND_TRAINING);
+        getModel().addTraining(new TrainingBuilder().withDateTime(VALID_LOCAL_DATE_TIME_3).build());
+        getModel().addTraining(new TrainingBuilder().withDateTime(VALID_LOCAL_DATE_TIME_2).build());
         CommandResult commandResult = addAllStudentToTrainingCommand.execute(getModel());
         assertEquals(String.format(AddAllStudentToTrainingCommand.MESSAGE_ADD_STUDENT_SUCCESS,
-                "1, 2, 3, 4"),
+                "1, 2, 3, 4, 5, 6, 7"),
                 commandResult.getFeedbackToUser());
         //Student 1 should already have dateTime in his field
         assertTrue(getModel().getFilteredStudentList().get(0)
@@ -91,16 +104,17 @@ public class AddAllStudentToTrainingCommandTest {
                 .containsAttendance(new Attendance(VALID_LOCAL_DATE_TIME_2)));
 
         //Four students inside of the training container
-        assertTrue(getModel().getFilteredTrainingList().get(3).getStudents().size() == 4);
+        assertTrue(getModel().getFilteredTrainingList().get(1).getStudents().size() == 7);
         //Training class should contain student1 too
-        assertTrue(getModel().getFilteredTrainingList().get(3).getStudents().contains(getModel()
+        assertTrue(getModel().getFilteredTrainingList().get(1).getStudents().contains(getModel()
                 .getFilteredStudentList().get(0)));
         //Training class should contain student2 too
-        assertTrue(getModel().getFilteredTrainingList().get(3).getStudents().contains(getModel()
+        assertTrue(getModel().getFilteredTrainingList().get(1).getStudents().contains(getModel()
                 .getFilteredStudentList().get(1)));
         //Training class should contain student3 too
-        assertTrue(getModel().getFilteredTrainingList().get(3).getStudents().contains(getModel()
+        assertTrue(getModel().getFilteredTrainingList().get(1).getStudents().contains(getModel()
                 .getFilteredStudentList().get(2)));
+        resetModel();
     }
 
     @Test
@@ -128,4 +142,11 @@ public class AddAllStudentToTrainingCommandTest {
         assertFalse(addAllStudent1Command.equals(addAllStudent2Command));
     }
 
+    void resetModel() {
+        List<Student> studentList = getModel().getFilteredStudentList();
+        List<Id> idList = studentList.stream()
+                .map(Student::getId)
+                .collect(Collectors.toList());
+        new DeleteStudentFromTrainingCommand(INDEX_SECOND_TRAINING, idList);
+    }
 }
